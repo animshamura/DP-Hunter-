76a991940271ac584cb4b670e88f6113c89b59c6
Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-08-21 02:02:41 +0600

    Create Floyd-Warshall-Vector-Unknown-Vertices.cpp

---
+++ Floyd Warshall/Floyd-Warshall-Vector-Unknown-Vertices.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define inf 9999
+using namespace std;
+
+vector<vector<int> >dist;
+
+void Warshall(int n){
+	for(int k=0;k<n;k++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(dist[i][j] > dist[i][k]+dist[k][j]){
+					dist[i][j] = dist[i][k] + dist[k][j];
+					
+				}
+			}
+		}
+	}
+}
+
+
+
+void Print(int n){
+    
+	for(int i=0;i<n;i++){
+	    
+		for(int j=0;j<n;j++){
+		    
+			if(dist[i][j] != inf){
+			    
+			    cout<<"inf"<<" ";
+			}
+			
+			else cout<<dist[i][j]<<" ";
+				
+		}
+		cout<<endl; 
+	}
+}
+int main(){
+    int v,t;
+    cout<<"Enter number of vertex : ";
+	cin>>v;
+  dist.resize(v);
+	for(int i=0;i<v;i++){
+	    
+	dist[i].resize(v);  
+	
+	for(int j=0;i<v;j++){
+	   
+     	cin>>dist[i][j];
+     	
+		}
+	}
+	Warshall(v);
+	Print(v);
+}
